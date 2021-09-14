70373a0ca0834244fb59496b3492434da2ab6bff
GL1NAM3SS <voinkrygona.ua@gmail.com>
2021-09-14 16:15:58 +0300

    ss

---
+++ learning_log/learning_log/learning_logs/urls.py
@@ -0,0 +1,25 @@
+"""learning_log URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('hui/', views.hui),
+    path('new_topic/',views.new_topic, name='new_topic'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:pk>', views.TopicsDetailView.as_view(), name='topics_detail'),
+    path('topics/<int:pk>/update', views.TopicsUpdateView.as_view(), name='topics_update')
+]
